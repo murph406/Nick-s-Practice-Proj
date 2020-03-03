0eb987a12a9a3783dddf78b4ff75fd28a2d752c7
Ryan Murphy <ryanmurphy125@gmail.com>
2020-03-03 09:10:41 -0800

    Instructions

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class HomeScreen extends Component {
+class HomeScreen extends Component {
+
+  /*
+  1. Add state to this Component that is set to empty array
+  2. Create a text box and button that adds a list or value to the state 
+  3. Somehow pass that state to the listScreen and display that info there 
+  */
+
   render() {
     return (
       <View style={styles.container}>
@@ -11,10 +18,12 @@ export default class HomeScreen extends Component {
   }
 }
 
+export default HomeScreen;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,5 @@
+/*
+Create a screen that takes the input from homeScreen and display's
+that info in a list that scrollable (Use Flatlist)
+make the component classless or functional (without state)
+*/
